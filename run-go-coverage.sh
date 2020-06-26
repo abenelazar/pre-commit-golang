7e4e6779f37ded61c37dc3b6db4b6620681a9f05
Ari Ben-Elazar <abenelazar@gmail.com>
2020-06-26 16:56:13 -0400

    test

--- run-go-coverage.sh
+++ run-go-coverage.sh
@@ -2,13 +2,15 @@
 
 if [ $# -ne 1 ]; then
   echo "accepts 1 param"
-  echo $1
   echo $2
   echo $3
   echo $4
   echo $5
-  exit 1
 fi
+
+THRESHOLD_PARAM=$1
+THRESHOLD=${THRESHOLD_PARAM[1]}
+
 
 cov=$(go tool cover -func coverage.out | grep total | awk '{print substr($3, 1, length($3)-3)}')
 if [[ $1 -gt $cov ]]; then
