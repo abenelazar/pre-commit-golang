d5dbcb6802229a8cf6d1e468dcef6c451ca7a7ac
Ari Ben-Elazar <abenelazar@gmail.com>
2020-06-26 16:48:50 -0400

    test

--- run-go-coverage.sh
+++ run-go-coverage.sh
@@ -6,7 +6,7 @@ if [ $# -ne 1 ]; then
 fi
 
 cov=$(go tool cover -func coverage.out | grep total | awk '{print substr($3, 1, length($3)-3)}')
-if [ $1 -gt $cov ]; then
+if [[ $1 -gt $cov ]]; then
     echo "coverage is below the threshold " $1 ". got " $COVERAGE
     exit 1
 fi
