6a92f66f2dd432c824c6149fdd8e2330bfcfac89
Ari Ben-Elazar <abenelazar@gmail.com>
2020-06-26 16:56:58 -0400

    test

--- run-go-coverage.sh
+++ run-go-coverage.sh
@@ -11,10 +11,12 @@ fi
 THRESHOLD_PARAM=$1
 THRESHOLD=${THRESHOLD_PARAM[1]}
 
+echo $THRESHOLD
+echo $THRESHOLD_PARAM
 
 cov=$(go tool cover -func coverage.out | grep total | awk '{print substr($3, 1, length($3)-3)}')
-if [[ $1 -gt $cov ]]; then
-    echo "coverage is below the threshold " $1 ". got " $COVERAGE
+if [[ $THRESHOLD -gt $cov ]]; then
+    echo "coverage is below the threshold " $THRESHOLD ". got " $COVERAGE
     exit 1
 fi
 
