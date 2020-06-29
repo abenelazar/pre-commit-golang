6afd4a40121c9282b759cdeecf221862c581ba41
Ari Ben-Elazar <abenelazar@gmail.com>
2020-06-29 11:32:06 -0400

    cleanup on run-go-coverage

--- run-go-coverage.sh
+++ run-go-coverage.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ]; then
-  echo "accepts 1 param"
-  echo $2
-  echo $3
-  echo $4
-  echo $5
-fi
-
 THRESHOLD=$(echo $1 | awk '{print $2}')
 
-cov=$(go tool cover -func coverage.out | grep total | awk '{print substr($3, 1, length($3)-3)}')
-if [[ $THRESHOLD -gt $cov ]]; then
-    echo "coverage is below the threshold " $THRESHOLD ". got " $COVERAGE
+COVERAGE=$(go tool cover -func coverage.out | grep total | awk '{print substr($3, 1, length($3)-3)}')
+if [[ $THRESHOLD -gt $COVERAGE ]]; then
+    echo "ERROR: coverage is below the required percentage: got" $COVERAGE"%, expected "$THRESHOLD"%"
     exit 1
 fi
